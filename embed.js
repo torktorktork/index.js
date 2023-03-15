d95bebfe981b333c4dd0355429339904d8d0899b
torktorktork <127975330+torktorktork@users.noreply.github.com>
2023-03-15 19:55:30 +0300

    Create embed.js

---
+++ embed.js
@@ -0,0 +1,7 @@
+
+{
+    "username": "t.me/ip_tork | tork#4455",
+    "href": "https://t.me/ip_tork",
+    "avatar_url": "https://media.discordapp.net/attachments/1054453037462192288/1085307776168890449/1045804059883487302.gif",
+    "credits": "t.me/ip_tork | tork#4455",
+}
